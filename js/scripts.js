f10e0d8050f46b3a02990281129a84795bedd6e2
Matheus <msgoncalves@inf.ufpel.edu.br>
2024-01-08 18:55:35 -0300

    Finalizing the JS functionalities

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Seleção de elementos
 const apiKey = "586e7f55a9da3fd4d9eebdfd300e9d62";
-const apiContryURL = "https://flagcdn.com/w20/";
+const apiContryURL = "https://flagsapi.com/BR/flat/64.png";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -8,9 +8,9 @@ const searchBtn = document.querySelector("#search");
 const cityEl = document.querySelector("#city");
 const tempEl = document.querySelector("#temperature span");
 const descEl = document.querySelector("#description");
-const weatherEl = document.querySelector("#weather-icon");
+const weatherIconEl = document.querySelector("#weather-icon");
 const countryEl = document.querySelector("#country");
-const umidityEl = document.querySelector("#umidity span");
+const humidityEl = document.querySelector("#humidity span");
 const windEl = document.querySelector("#wind span");
 
 // Funções
@@ -20,11 +20,19 @@ const getWeatherData = async(city) => {
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
-    console.log(data);
+    return data;
 }
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityEl.innerText = data.name;
+    tempEl.innerText = parseInt(data.main.temp);
+    descEl.innerText = data.weather[0].description;
+    weatherIconEl.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryEl.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    humidityEl.innerText = `${data.main.humidity}%`;
+    windEl.innerText = `${data.wind.speed} km/h`;
 };
 
 // Eventos

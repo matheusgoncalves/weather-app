5144c2b0d209b7f54f61079eb1e1d95e189abf45
Matheus <msgoncalves@inf.ufpel.edu.br>
2024-01-08 18:18:33 -0300

    Setting API Weather request function

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,26 @@ const apiContryURL = "https://flagcdn.com/w20/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityEl = document.querySelector("#city");
+const tempEl = document.querySelector("#temperature span");
+const descEl = document.querySelector("#description");
+const weatherEl = document.querySelector("#weather-icon");
+const countryEl = document.querySelector("#country");
+const umidityEl = document.querySelector("#umidity span");
+const windEl = document.querySelector("#wind span");
+
 // Funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    console.log(data);
+}
+
 const showWeatherData = (city) => {
-    console.log(city);
+    getWeatherData(city);
 };
 
 // Eventos
